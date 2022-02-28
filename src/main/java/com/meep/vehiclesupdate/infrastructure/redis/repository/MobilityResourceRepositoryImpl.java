552a52da7b8442cfdd5f81d286725078e915eae7
Cristobal Hermida Portales <cristobalhp@ext.zara.com>
2022-02-28 08:55:38 +0100

    add creationdate to model

--- src/main/java/com/meep/vehiclesupdate/infrastructure/redis/repository/MobilityResourceRepositoryImpl.java
+++ src/main/java/com/meep/vehiclesupdate/infrastructure/redis/repository/MobilityResourceRepositoryImpl.java
@@ -4,6 +4,7 @@ import com.meep.vehiclesupdate.domain.entity.MobilityResource;
 import com.meep.vehiclesupdate.domain.exception.MobilityResourceRepositoryException;
 import com.meep.vehiclesupdate.domain.repository.MobilityResourceRepository;
 import com.meep.vehiclesupdate.infrastructure.redis.mapper.MobilityResourceModelMapper;
+import java.time.OffsetDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
@@ -35,7 +36,9 @@ public class MobilityResourceRepositoryImpl implements MobilityResourceRepositor
       var modelOptional = repository.findById(mobilityResource.getId());
       if (modelOptional.isEmpty()) {
         log.info("updateVehicle saved for vehicle id: {} ", mobilityResource.getId());
-        repository.save(mapper.asVehicleModel(mobilityResource));
+        var model= mapper.asVehicleModel(mobilityResource);
+        model.setCreationDate(OffsetDateTime.now());
+        repository.save(model);
       }
     } catch (Exception e) {
       throw new MobilityResourceRepositoryException(String.format("Error updating vehicle with Id [%s]", mobilityResource.getId()), e);
